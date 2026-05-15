c73de7f999eb981f0bcc830586b605e8921de278
gwamaka1 <mwamtobegwamaka@gmail.com>
2026-05-15 17:31:58 -0500

    finished with test cases

--- src/main/java/com/pluralsight/NameFormatter.java
+++ src/main/java/com/pluralsight/NameFormatter.java
@@ -1,4 +1,36 @@
 package com.pluralsight;
 
 public class NameFormatter {
+    private NameFormatter(){
+
+    }
+    public static String format(String firstName, String lastName){
+        return lastName+","+" "+firstName;
+
+    }
+    public static String format(String prefix,String firstName, String middleName, String lastName,String suffix){
+        return lastName+","+" "+prefix+" "+firstName+" "+ middleName+","+" "+ suffix;
+
+    }
+    public static String format(String fullName){
+        String [] parts = fullName.split(" ");
+
+
+        if (parts.length ==2){
+           return format(parts[0],parts[1].replace(",",""));
+        }
+        else if (parts.length ==5) {
+           return format(parts[0],parts[1],parts[2],parts[3].replace(",",""),parts[4]);
+
+        }
+        else if (parts.length==4){
+            return parts[2].replace(",","")+","+" "+parts[0]+" "+parts[1]+","+" "+ parts[3];
+
+        }
+        return "";
+
+
+    }
+
 }
+
--- src/test/java/com/pluralsight/NameFormatterTest.java
+++ src/test/java/com/pluralsight/NameFormatterTest.java
@@ -1,4 +1,31 @@
+package com.pluralsight;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class NameFormatterTest {
-  
+    @Test
+    public void format_TwoNames_ReturnsLastNameCommaFirstName(){
+        //Act and Arrange
+        String ans = NameFormatter.format("maka","mwamtobe");
+        //Assert
+        assertEquals("mwamtobe, maka",ans);
+    }
+    @Test
+    public void format_FullNameWithPrefixMiddleNameAndSuffix_ReturnLastNameCommaPrefixFirstNameMiddleNameCommaSuffix(){
+        //Act and Arrange
+        String ans = NameFormatter.format("Dr.","maka","B","mwamtobe","PhD");
+        //Assert
+        assertEquals("mwamtobe, Dr. maka B, PhD",ans);
+    }
+    @Test
+    public void format_FullNameWithMiddleNameAndSuffix_ReturnsLastNameCommaFirstNameMiddleNameCommaSuffix() {
+        // Act and Arrange
+        String ans = NameFormatter.format("maka B mwamtobe, PhD");
+
+        // Assert
+        assertEquals("mwamtobe, maka B, PhD", ans);
+    }
+
 }
